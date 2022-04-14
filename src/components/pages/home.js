7ce5f9c65e1962b8586af64b377e51991b0ddd04
Alexander Smith <alex.smith4640@yahoo.com>
2022-04-14 18:46:21 -0500

    added images and flexboxes to homepage

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -17,6 +17,30 @@ export default class Home extends Component {
            
             <div className="page-wrapper">
                 <Header/>
+
+                <div className="content-wrapper">
+                    <div className="left-column">
+                        <div className="image-wrapper">
+                            <img src="https://source.unsplash.com/random/?auto"/>
+                       </div>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, ratione. Aut dolore obcaecati, illo aliquid optio nobis eum commodi dolores, 
+                            dignissimos, alias ad odit non voluptates dolor consequatur ullam facilis sunt porro esse enim. Nesciunt.
+                        </p>
+                    </div>
+
+                    <div className="right-column">
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores sed dignissimos? Non dolor sapiente perferendis quia adipisci incidunt, consectetur iure repellat amet eaque nemo quisquam
+                            laudantium ullam quo asperiores cumque, architecto saepe possimus consequatur quos reiciendis. Dicta, excepturi libero!
+                        </p>
+                        <div className="image-wrapper">
+                            <img src="https://source.unsplash.com/random/?autoshop"/>
+                       </div>
+
+                        
+                    </div>
+
+                </div>
             </div>
         )
     }
