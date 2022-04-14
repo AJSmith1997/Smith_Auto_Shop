fc2c5a923141c6187cfa6e4847472bf623b3cf70
Alexander Smith <alex.smith4640@yahoo.com>
2022-04-14 18:06:23 -0500

    initial commit and added navbar

---
+++ src/components/header.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+
+
+
+export default function Header() {
+    return(
+       
+        
+       
+        <div className="header">
+
+                   
+                
+            <div className="background">
+               
+                    
+               
+                <div className="title">
+                    <h1>Smith's Tire and Auto</h1>
+                </div>
+
+
+                
+                <div className="navbar">
+
+
+                    <button>
+                        <div className="link1">
+                            <h3>Services</h3>
+                        </div>
+                    </button>
+                    
+                    <button>
+                        <div className="link2">
+                            <h3>About Us</h3>
+                        </div>
+                    </button>
+
+                    <button>
+                        <div className="link3">
+                            <h3>Testimonials</h3>
+                        </div>
+                    </button>
+
+                    <button>
+                        <div className="link4">
+                            <h3>Contact Us</h3>
+                        </div>
+                    </button>    
+                </div>
+                 
+               
+            </div>
+        </div>
+    )
+}
---
+++ src/components/pages/home.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react"
+
+import Header from "../header"
+
+export default class Home extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        
+        return (
+           
+            <div className="page-wrapper">
+                <Header/>
+            </div>
+        )
+    }
+}
